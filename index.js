2053afbfa1588aee3b43787e12553b35b6c30200
Negar234 <156413941+Negar234@users.noreply.github.com>
2024-10-28 01:19:29 +0100

    inlämning 4

--- index.js
+++ index.js
@@ -6,3 +6,45 @@ function setTimer() {
     var d = new Date()
     document.getElementById("timer").innerHTML = d.toLocaleTimeString()
 }
+
+var leftSide = -1050
+function showRollingPics() {
+    document.getElementById("rollingPicsContainer").style.display = "block"
+
+    $(this).animate({ left: "1000px" })
+
+}
+
+var isRollingOpen = false
+
+$(document).ready(function () {
+
+    $("#tabDiv").on("click", function () {
+
+        if (!isRollingOpen) {
+            if ($(window).width() > 600) {
+
+                $("#newPanel").animate({ left: "0px" }, 1000)
+            }
+            else {
+                $("#newPanel").animate({ top: "0px" }, 1000)
+            }
+
+            isRollingOpen = true
+        }
+        else {
+            if ($(window).width() > 600) {
+
+                $("#newPanel").animate({ left: "-1000px" }, 1000)
+            }
+            else {
+                $("#newPanel").animate({ top: "-620px" }, 1000)
+            }
+
+            isRollingOpen = false
+        }
+
+    })
+
+
+})
